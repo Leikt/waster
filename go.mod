c9162915f23025d969914947a406c388f9f54ba3
Leikt Sol'Reihin <leikt.solreihin@gmail.com>
2024-01-30 20:08:07 +0100

    feat: initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Leikt/waster
+
+go 1.21.6
---
+++ tools/setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Set up the local project for development
+
+# Change the location of the git hooks
+git config core.hooksPath githooks
